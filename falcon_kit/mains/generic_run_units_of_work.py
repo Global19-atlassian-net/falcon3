80a3fd6d42435b2ebb7f8c0026e6c8b07ad6abfa
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-06-05 11:32:21 -0700

    Run generic_units_of_work in parallel

--- falcon_kit/mains/generic_run_units_of_work.py
+++ falcon_kit/mains/generic_run_units_of_work.py
@@ -1,5 +1,4 @@
-
-
+from multiprocessing import cpu_count
 import argparse
 import collections
 import glob
@@ -7,7 +6,10 @@ import logging
 import os
 import sys
 import pypeflow.do_task
+
+from ..multiproc import Pool
 from .. import io
+from ..util.io import run_func
 
 LOG = logging.getLogger()
 
@@ -36,12 +38,29 @@ def update_values_rel_to(things, dn):
             # Probably just not a string. But could be str, unicode, ...
             pass
 
-def run(bash_template_fn, units_of_work_fn, nproc,
-        results_fn):
+def run_uow(uow_dir, script, inputs, outputs, params):
+        io.rmdir(uow_dir)
+        io.mkdirs(uow_dir)
+        with io.cd(uow_dir):
+            pypeflow.do_task.run_bash(script, inputs, outputs, params)
+            resolved_outputs = {k: os.path.abspath(v) for k,v in list(outputs.items())}
+        return {k: os.path.join('.', os.path.relpath(v)) for k,v in list(resolved_outputs.items())}
+        # Must be relative to this dir.
+        # (We assume outputs are under the current directory.)
+        # The reason for the './' prefix? So we can substitute in CWD later,
+        # in case we ran in /tmp. This also helps the pbsmrtpipe "gatherer".
+
+        #wildcards_str = '_'.join(w for w in itervalues(job['wildcards']))
+        #job_name = 'job{}'.format(wildcards_str)
+        #for (output_name, output_fn) in viewitems(outputs):
+        #    giname = '{}_{}'.format(job_name, output_name)
+        #    gather_inputs[giname] = output_fn
+
+def yield_uows(units_of_work_fn, bash_template_fn, nproc):
     units_of_work_fn = os.path.realpath(units_of_work_fn)
+    script = open(bash_template_fn).read()
     uows = io.deserialize(units_of_work_fn)
-    uow_dirs = list()
-    results = list()
+
     for i, uow in enumerate(uows):
         uow_dir = 'uow-{:02d}'.format(i)
         rel_units_of_work_dn = os.path.normpath(os.path.relpath(os.path.dirname(units_of_work_fn), uow_dir))
@@ -55,27 +74,40 @@ def run(bash_template_fn, units_of_work_fn, nproc,
         # matches pypeflow_nproc.
 
         #params.update({k: v for (k, v) in viewitems(job['wildcards'])}) # include expanded wildcards
-        LOG.info('INPUT:{}'.format(inputs))
-        LOG.info('OUTPUT:{}'.format(outputs))
-        LOG.info('PARAMS:{}'.format(params))
-        uow_dirs.append(uow_dir)
-        io.rmdir(uow_dir)
-        io.mkdirs(uow_dir)
-        script = open(bash_template_fn).read()
-        with io.cd(uow_dir):
-            pypeflow.do_task.run_bash(script, inputs, outputs, params)
-            resolved_outputs = {k: os.path.abspath(v) for k,v in list(outputs.items())}
-        results.append({k: os.path.join('.', os.path.relpath(v)) for k,v in list(resolved_outputs.items())})
-        # Must be relative to this dir.
-        # (We assume outputs are under the current directory.)
-        # The reason for the './' prefix? So we can substitute in CWD later,
-        # in case we ran in /tmp. This also helps the pbsmrtpipe "gatherer".
+        LOG.debug('----')
+        LOG.debug('INPUT:{}'.format(inputs))
+        LOG.debug('OUTPUT:{}'.format(outputs))
+        LOG.debug('PARAMS:{}'.format(params))
+        #uow_dirs.append(uow_dir)
+        yield (run_uow, uow_dir, script, inputs, outputs, params)
 
-        #wildcards_str = '_'.join(w for w in itervalues(job['wildcards']))
-        #job_name = 'job{}'.format(wildcards_str)
-        #for (output_name, output_fn) in viewitems(outputs):
-        #    giname = '{}_{}'.format(job_name, output_name)
-        #    gather_inputs[giname] = output_fn
+def run(bash_template_fn, units_of_work_fn, nproc, nproc_per_uow,
+        results_fn):
+    if nproc_per_uow < 1:
+        LOG.error('nproc_per_uow == {} < 1; using 1 instead'.format(nproc_per_uow))
+        nproc_per_uow = 1
+    inputs = list(yield_uows(units_of_work_fn, bash_template_fn, nproc_per_uow))
+
+    njobs = (nproc // nproc_per_uow) if (nproc > 0) else (cpu_count() // nproc)
+    LOG.info('For multiprocessing, parallel njobs={} (cpu_count={}, nproc={}, nproc_per_uow={})'.format(
+        njobs, cpu_count(), nproc, nproc_per_uow))
+
+    results = list()
+
+    def Start():
+        LOG.info('Started a worker in {} from parent {}'.format(
+            os.getpid(), os.getppid()))
+    exe_pool = Pool(njobs, initializer=Start)
+    try:
+        LOG.info('running {} units-of-work, {} at a time...'.format(len(inputs), njobs))
+        for res in exe_pool.imap(run_func, inputs):
+            results.append(res)
+        LOG.info('finished {} units-of-work'.format(len(inputs)))
+    except:
+        LOG.exception('failed multiprocessing')
+        exe_pool.terminate()
+        raise
+
     io.serialize(results_fn, results)
 
 
@@ -92,8 +124,11 @@ def parse_args(argv):
         formatter_class=HelpF,
     )
     parser.add_argument(
-        '--nproc',
-        help='Number of processors to be used.')
+        '--nproc', type=int, default=0,
+        help='Number of processors allowed to be used (0 => {}).'.format(cpu_count()))
+    parser.add_argument(
+        '--nproc-per-uow', type=int, default=1,
+        help='Number of processors expected to be used by each units-of-work (num parallel procs will be nproc/nproc-per-uow)')
     parser.add_argument(
         '--bash-template-fn',
         help='Input. Template of bash script to run on each unit-of-work, with snakemake-style substitutions.')
