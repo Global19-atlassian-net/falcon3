b15ed31c6bf29c90afb1396d36843d772251aa8c
Christopher Dunn <cdunn@pacificbiosciences.com>
2019-09-26 19:41:08 -0700

    fixup! noop.py: for debugging

--- falcon_kit/mains/noop.py
+++ falcon_kit/mains/noop.py
@@ -4,7 +4,7 @@ def run(prog, *args):
     print("{} on {!r}".format(prog, args))
 
 def main(argv=sys.argv):
-    run(*argv)
+    run(*argv) # pylint: disable=no-value-for-parameter
 
 if __name__ == '__main__':  # pragma: no cover
     main()
